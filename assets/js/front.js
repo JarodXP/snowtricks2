8102c8a1689bb4970ebaca9d1f3b2b5e6e2d1017
jarod <gregory@jarod-xp.com>
2020-03-02 16:11:10 +0100

    Twig Layout template

---
+++ assets/js/front.js
@@ -0,0 +1,32 @@
+const desktopNav = document.getElementById('desktop-front-nav');
+const mobileNav = document.getElementById('mobile-front-nav');
+
+//Allows to use FontAwesome in Pseudo-elements
+window.FontAwesomeConfig = {
+    searchPseudoElements: true
+};
+
+//Event listener for toggling nav bar
+window.addEventListener("resize", toggleNav);
+window.onload = toggleNav();
+
+//Toggles type of menu depending on window size
+function toggleNav() {
+//Toggles the ".hidden" class
+    if(document.body.clientWidth < 576){
+        if(!desktopNav.classList.contains('hidden')){
+            desktopNav.classList.add('hidden');
+        }
+        if(mobileNav.classList.contains('hidden')){
+            mobileNav.classList.remove('hidden')
+        }
+    }
+    else{
+        if(!mobileNav.classList.contains('hidden')){
+            mobileNav.classList.add('hidden');
+        }
+        if(desktopNav.classList.contains('hidden')){
+            desktopNav.classList.remove('hidden')
+        }
+    }
+}
