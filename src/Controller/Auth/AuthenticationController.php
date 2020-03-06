be06f3189c9ba86cc0206e5d689857e535f2a945
jarod <gregory@jarod-xp.com>
2020-03-06 14:02:54 +0100

    AuthenticationController updated

--- src/Controller/Auth/AuthenticationController.php
+++ src/Controller/Auth/AuthenticationController.php
@@ -11,11 +11,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class AuthenticationController extends AbstractController
 {
     /**
-     * @Route("/sign-up",name="sign_up")
+     * @Route("/auth/sign-up",name="sign_up")
      * @return Response
      */
     public function displaySignUpAction():Response
     {
         return $this->render('auth\sign_up.html.twig');
+    }
+
+    /**
+     * @Route("/auth/sign-in",name="sign_in")
+     * @return Response
+     */
+    public function displaySignInAction():Response
+    {
+        return $this->render('auth\sign_in.html.twig');
+    }
+
+    /**
+     * @Route("/auth/forgot-password",name="forgot-password")
+     * @return Response
+     */
+    public function displayForgotPasswordAction():Response
+    {
+        return $this->render('auth\forgot_password.html.twig');
+    }
+
+    /**
+     * @Route("/auth/reset-password",name="reset-password")
+     * @return Response
+     */
+    public function displayResetPasswordAction():Response
+    {
+        return $this->render('auth\reset_password.html.twig');
     }
 }
