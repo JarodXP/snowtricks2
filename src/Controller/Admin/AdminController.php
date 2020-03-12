c2b8f3a0f9246c457bf7be1d3112b29ec02f0cc1
jarod <gregory@jarod-xp.com>
2020-03-12 15:50:11 +0100

    Security bundle implemented and authorisation set for Admin Controller

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -5,25 +5,29 @@ namespace App\Controller\Admin;
 
 use App\Entity\Trick;
 use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class AdminController
+ * @package App\Controller\Admin
+ */
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin/tricks",name="admin-tricks")
      * @return Response
      */
-    public function displayTrickListAction(string $user = 'Wawa'):Response
+    public function displayTrickListAction():Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN','Access Denied!!');
         $tricks = $this->getDoctrine()
             ->getRepository(Trick::class)
             ->findAll();
 
-        $user = $this->getDoctrine()
-            ->getRepository(User::class)
-            ->findOneBy(['username' => $user]);
+        $user = $this->getUser();
 
         return $this->render('admin\trick_list.html.twig', [
             'tricks' => $tricks,
