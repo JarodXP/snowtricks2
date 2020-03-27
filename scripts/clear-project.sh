800004cd17b43bb426fdb88f9a6aff4e7242e40c
jarod <gregory@jarod-xp.com>
2020-03-27 17:48:33 +0100

    Appspec updated for test domain

--- scripts/clear-project.sh
+++ scripts/clear-project.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /var/www/snowtricks || exit
+cd /var/www/snowtricks-test || exit
 find . ! -name '.env.local' -type d -exec rm -r {} +
 find . ! -name '.env.local' -type f -exec rm -f {} +
--- scripts/permissions.sh
+++ scripts/permissions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Sets permissions
-sudo find /var/www/snowtricks/ -type d -exec chmod 755 {} +
-sudo find /var/www/snowtricks/ -type f -exec chmod 644 {} +
+sudo find /var/www/snowtricks-test/ -type d -exec chmod 755 {} +
+sudo find /var/www/snowtricks-test/ -type f -exec chmod 644 {} +
 
-#Set var log/open permission
-chmod -R 777 /var/www/snowtricks/var/log/
-chmod -R 777 /var/www/snowtricks/var/cache/
+#Set var/log/ & var/cache permission
+chmod -R 777 /var/www/snowtricks-test/var/log/
+chmod -R 777 /var/www/snowtricks-test/var/cache/
