759d55da5c35ed196cea47af40727b6dfa30472c
GBA <gregory@jarod-xp.com>
2020-04-14 15:50:13 +0200

    Registration Form updated

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Controller\SecurityController;
 use App\Entity\User;
+use App\Form\CustomType\CustomEmailType;
+use App\Form\CustomType\PasswordRepeatedType;
+use App\Form\CustomType\UsernameType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -19,8 +22,8 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add(SecurityController::USERNAME_FIELD)
-            ->add(SecurityController::EMAIL_FIELD)
+            ->add(SecurityController::USERNAME_FIELD,UsernameType::class)
+            ->add(SecurityController::EMAIL_FIELD,CustomEmailType::class)
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -29,29 +32,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('password-group', RepeatedType::class, [
-                'mapped' => false,
-                'type' => PasswordType::class,
-                'invalid_message' => 'Both password fields must be identical',
-                'first_name' => SecurityController::PASSWORD_FIELD,
-                'first_options' => ['label' => 'Password'],
-                'second_name' => SecurityController::PASSWORD_CHECK_FIELD,
-                'second_options' => ['label' => 'Re-type password'],
-                'options' => [
-                    'mapped' => 'false',
-                    'constraints' => [
-                        new NotBlank([
-                            'message' => 'Please enter a password',
-                        ]),
-                        new Length([
-                            'min' => 6,
-                            'minMessage' => 'Your password should be at least {{ limit }} characters',
-                            // max length allowed by Symfony for security reasons
-                            'max' => 12,
-                        ]),
-                    ],
-                ]
-            ]);
+            ->add('passwordGroup', PasswordRepeatedType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
