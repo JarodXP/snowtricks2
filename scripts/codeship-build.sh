37a3d83d63af9a09e73b0eef4488aca09ea113fb
jarod <gregory@jarod-xp.com>
2020-05-12 16:39:49 +0200

    Bug fix in codeship-build.sh bis

--- scripts/codeship-build.sh
+++ scripts/codeship-build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #Creates a .env.local file
-
 if [ "$CI_BRANCH" = "master" ]
 then
   environment='prod'
@@ -9,7 +8,7 @@ else
   environment='dev'
 fi
 
-printf "%s" "APP_ENV=" "$environment" "\nDATABASE_URL=" "$DATABASE_URL" > .env.local
+printf "%s\n" "APP_ENV=$environment" "DATABASE_URL=$DATABASE_URL" > .env.local
 
 
 #Changes the name of the appspec file depending on the branch
