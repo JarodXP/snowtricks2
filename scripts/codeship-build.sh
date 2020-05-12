cb20e1b2f58fd4822fa86be4839696c3f1895b1e
jarod <gregory@jarod-xp.com>
2020-05-12 16:11:03 +0200

    CodeShip script updated to create .env.local file

--- scripts/codeship-build.sh
+++ scripts/codeship-build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 #Creates a .env.local file
-touch .env.local
-"$CI_BRANCH"
+cd /var/www/snowtricks-test || exit
+
+if [ "$CI_BRANCH" = "master" ]
+then
+  environment='prod'
+else
+  environment='dev'
+fi
+
+printf "%s" "APP_ENV=" "$environment" "\nDATABASE_URL=" "$DATABASE_URL" > .env.local
+
 
 #Changes the name of the appspec file depending on the branch
 if [ "$CI_BRANCH" = "test" ]
